86cfb975d69b6af4b54c89009a308cee256b0d3f
THAMBI <manoharan.thambi@lhind.dlh.de>
2019-06-27 22:53:23 +0200

    Content type changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/json');
     res.end('Hello World\n');
 });
 server.listen(port, () => {
